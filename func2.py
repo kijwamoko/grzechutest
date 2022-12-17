ae8090260556bf6b20428ee47d595657e73a56f3
Grzesiek <skiba>
2022-12-17 08:13:13 +0100

    modify func2 and create main.py

--- func2.py
+++ func2.py
@@ -1,2 +1,5 @@
 def func123(x, y):
     return x+y
+
+def concatenate_lists(l1,l2):
+    return l1 + l2
---
+++ main.py
@@ -0,0 +1,8 @@
+import func2
+
+l1 = [1,3,5,9]
+l2 = [2,4,6,8,10]
+
+l3 = func2.concatenate_lists(l1,l2)
+
+print(l3)
